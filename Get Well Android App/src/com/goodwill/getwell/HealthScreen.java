5cd222697aba0f7758653e3cccfcd0caf1eb3ab9
knigam <kushal.nigam1@gmail.com>
2013-10-05 06:07:19 -0400

    Added spinner to health layout

--- Get Well Android App/src/com/goodwill/getwell/HealthScreen.java
+++ Get Well Android App/src/com/goodwill/getwell/HealthScreen.java
@@ -5,14 +5,45 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class HealthScreen extends Activity{
+	
+	private String[] challenges;
+	String challenge1;
+	String challenge2;
+	String challenge3;
+	String challenge4;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.health_screen);
-	}
+		challenge1 = "test1";
+		challenge2 = "test2";
+		challenge3 = "test3";
+		challenge4 = "test4";
+		challenges = new String[]{"--choose a challenge--", challenge1, challenge2, challenge3, challenge4};
+		Spinner healthSpinner = (Spinner) findViewById(R.id.healthSpinner);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(HealthScreen.this, android.R.layout.simple_spinner_item, challenges);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    healthSpinner.setAdapter(adapter);
+		
+	    healthSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+	        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {               
+	            if(position != 0)
+	            	Toast.makeText(getApplicationContext(), challenges[position], Toast.LENGTH_SHORT).show();
+	    	}
+	
+	        public void onNothingSelected(AdapterView<?> parent) {
+	            // TODO Auto-generated method stub
+	        }});
+		}
 	
 	
 }
