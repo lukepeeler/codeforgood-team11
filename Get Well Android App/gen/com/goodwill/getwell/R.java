5037814bc1b37bd2ae4c52dc4f1360b05aa800bc
knigam <kushal.nigam1@gmail.com>
2013-10-05 00:23:58 -0400

    Added welcome screen layout and activity with buttons

--- Get Well Android App/gen/com/goodwill/getwell/R.java
+++ Get Well Android App/gen/com/goodwill/getwell/R.java
@@ -24,7 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int badgesBtn=0x7f080008;
+        public static final int careerBtn=0x7f080003;
+        public static final int currGlobalBtn=0x7f080004;
+        public static final int greetingView=0x7f080000;
+        public static final int healthBtn=0x7f080002;
+        public static final int moneyBtn=0x7f080005;
+        public static final int notificationsBtn=0x7f080006;
+        public static final int socialBtn=0x7f080001;
+        public static final int streakBtn=0x7f080007;
     }
     public static final class layout {
         public static final int welcome_screen=0x7f030000;
@@ -33,6 +42,7 @@ public final class R {
         public static final int welcome_screen=0x7f070000;
     }
     public static final class string {
+        public static final int UserName=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
