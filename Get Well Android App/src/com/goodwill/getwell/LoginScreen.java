319ff37c824100ce53db1b868c60936c589bb2cc
knigam <kushal.nigam1@gmail.com>
2013-10-05 11:07:00 -0400

    Got social working, login screen kind of works.

--- Get Well Android App/src/com/goodwill/getwell/LoginScreen.java
+++ Get Well Android App/src/com/goodwill/getwell/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.goodwill.getwell;
 
+import com.goodwill.getwell.databasemgr.DataBaseHelper;
 import com.goodwill.getwell.databasemgr.DatabaseManager;
 
 import android.os.Bundle;
@@ -7,17 +8,31 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.content.Context;
 
 public class LoginScreen extends Activity {
 	
 	int currStreak, currBadges, currNotifications;
-
+	DataBaseHelper helper;
+	protected Context context;
 	public static User user;
+	EditText usernameText, passwordText;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_screen);
-		user = DatabaseManager.fetchUserByUsername("foo");
+		System.out.println("TEST1");
+		helper = new DataBaseHelper(context);
+		System.out.println("TEST2");
+		user = helper.fetchUserByUsername("foo");
+		System.out.println("TEST 3");
+	}
+	private void usernameTextOnClick(View view){
+		
+	}
+	private void passwordTextOnClick(View view){
+		
 	}
 	
 }
