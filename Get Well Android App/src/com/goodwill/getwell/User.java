384df0fd90e4ae52aab039f1c437b2e9e784a880
lukepeeler <peeler.luke@gmail.com>
2013-10-05 00:53:56 -0400

    adding backend stuff

--- Get Well Android App/src/com/goodwill/getwell/User.java
+++ Get Well Android App/src/com/goodwill/getwell/User.java
@@ -28,6 +28,94 @@ public class User {
 		this.gender = gender;
 		this.employeeID = employeeID;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getHeightFeet() {
+		return heightFeet;
+	}
+
+	public void setHeightFeet(int heightFeet) {
+		this.heightFeet = heightFeet;
+	}
+
+	public int getHeightInches() {
+		return heightInches;
+	}
+
+	public void setHeightInches(int heightInches) {
+		this.heightInches = heightInches;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
 	
 	
 }
--- Get Well Android App/src/com/goodwill/getwell/databasemgr/DatabaseManager.java
+++ Get Well Android App/src/com/goodwill/getwell/databasemgr/DatabaseManager.java
@@ -1,32 +1,46 @@
 package com.goodwill.getwell.databasemgr;
 
 import java.io.File;
+
+import com.almworks.sqlite4java.SQLiteConnection;
+import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
 import com.goodwill.getwell.*;
 
-import org.tmatesoft.sqljet.core.SqlJetException;
-import org.tmatesoft.sqljet.core.table.SqlJetDb;
-
-
-
 public class DatabaseManager {
-	private String dbname = "appdb.db";
-	private File dbFile = new File(dbname);
-	private SqlJetDb db;
-	
-	public DatabaseManager()
-	{
-		try{
-			db = SqlJetDb.open(dbFile, true);
-		} catch (SqlJetException e){
+	private SQLiteConnection db;
+
+	public DatabaseManager() {
+		// TODO: edit path of this database when demoing!!
+		db = new SQLiteConnection(new File("../../../../../../appdb.db"));
+	}
+
+	public void addUser(User user) {
+		SQLiteStatement st = null;
+		try {
+			db.open(true);
+			st = db.prepare("INSERT INTO users (" + user.getUsername() + ", "
+					+ user.getPassword() + ", " + user.getEmail() + ", "
+					+ user.getFname() + ", " + user.getLname() + ", "
+					+ user.getAge() + ", " + user.getHeightFeet() + ", "
+					+ user.getHeightInches() + ", " + user.getWeight() + ", "
+					+ user.getGender() + ", " + user.getEmployeeID() + ");");
+			while (st.step()) {
+				;
+			}
+		} catch (SQLiteException e) {
 			e.printStackTrace();
+		} finally {
+			st.dispose();
 		}
 	}
-	
-	public void addUser(User user)
-	{
-		
+
+	public User fetchUserByUsername(String username) {
+		return null;
 	}
-	
-	
+
+	public static void main(String[] args) {
+		DatabaseManager dbm = new DatabaseManager();
+	}
 
 }
