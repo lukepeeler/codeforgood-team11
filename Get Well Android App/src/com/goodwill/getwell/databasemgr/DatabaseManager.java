3073bfae6d8843181813007ea26cf3478b425d37
lukepeeler <peeler.luke@gmail.com>
2013-10-05 06:10:50 -0400

    more db updates

--- Get Well Android App/src/com/goodwill/getwell/databasemgr/DatabaseManager.java
+++ Get Well Android App/src/com/goodwill/getwell/databasemgr/DatabaseManager.java
@@ -1,6 +1,13 @@
 package com.goodwill.getwell.databasemgr;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
 
 
 
@@ -10,14 +17,13 @@ import com.almworks.sqlite4java.SQLiteStatement;
 import com.goodwill.getwell.*;
 
 public class DatabaseManager {
-	private SQLiteConnection db;
+	private static SQLiteConnection db = new SQLiteConnection(new File("/Users/lukepeeler/dev/appdb.db"));
 
 	public DatabaseManager() {
 		// TODO: edit path of this database when demoing!!
-		db = new SQLiteConnection(new File("/Users/lukepeeler/dev/appdb.db"));
 	}
 
-	public void addUser(User user) {
+	public static void addUser(User user) {
 		SQLiteStatement st = null;
 		try {
 			db.open();
@@ -38,7 +44,7 @@ public class DatabaseManager {
 		db.dispose();
 	}
 
-	public User fetchUserByUsername(String username) {
+	public static User fetchUserByUsername(String username) {
 		SQLiteStatement st = null;
 		User user = null;
 		try {
@@ -63,9 +69,100 @@ public class DatabaseManager {
 			st.dispose();
 		}
 		
-		db.dispose();
+		//db.dispose();
 		return user;
 		
+	}
+	
+	public static ArrayList<Challenge> getUserChallenges(String username)
+	{
+		SQLiteStatement st = null;
+		Date created = null, due = null;
+		ArrayList<Challenge> challenges = new ArrayList<Challenge>();
+		try {
+			db.open();
+			st = db.prepare("SELECT * FROM challenge NATURAL JOIN participation WHERE username = '" + username +"';");
+			while (st.step()) {
+				try {
+					created = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(st.columnString(4));
+					due = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(st.columnString(5));
+				} catch (java.text.ParseException e) {
+					e.printStackTrace();
+				}
+				Challenge challenge = new Challenge(st.columnInt(0), st.columnString(1), st.columnString(2),
+													st.columnInt(3), created, due, st.columnString(6));
+				// see if challenge was completed (columnValue?)
+				if (st.columnInt(8) == 1){
+					challenge.markComplete();
+				}
+				
+				challenges.add(challenge);
+			}
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
+		
+		db.dispose();
+		return challenges;
+	}
+	
+	public static ArrayList<User> getUserFriends(String username)
+	{
+		SQLiteStatement st = null;
+		ArrayList<User> friends = new ArrayList<User>();
+		ArrayList<String> friendNames = new ArrayList<String>();
+		try {
+			db.open();
+			st = db.prepare("SELECT friend_name FROM users NATURAL JOIN friendship WHERE username = '" + username +"';");
+			while (st.step()) {
+				friendNames.add(st.columnString(0));
+			}
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
+		
+		//db.dispose();
+		
+		for (String name : friendNames){
+			friends.add(fetchUserByUsername(name));
+		}
+		return friends;
+	}
+	
+	public static void addFriendToUser(User user, String friendName)
+	{
+		SQLiteStatement st = null;
+		try{
+			db.open();
+			st = db.prepare("INSERT INTO friendship VALUES ('" + user.getUsername() + "', '" + friendName + "');");
+			while(st.step()){
+				;
+			}
+		} catch (SQLiteException e){
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
+	}
+	
+	public static void addChallengeToUser(String username, Challenge c)
+	{
+		SQLiteStatement st = null;
+		try{
+			db.open();
+			st = db.prepare("INSERT INTO participation VALUES (" + c.getChallengeID() + ", '" + username + "', " + 0 + ");");
+			while(st.step()){
+				;
+			}
+		} catch (SQLiteException e){
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
 	}
 		
 
